57c6e9c6fa0b598f2a7040b4675cc97086b657a3
Goran Ojkić <goran.ojkic@fyber.com>
2018-12-05 19:46:43 +0100

    init

--- core/shared/src/main/scala/org/zardina/Teams.scala
+++ core/shared/src/main/scala/org/zardina/Teams.scala
@@ -2,8 +2,16 @@ package org.zardina
 
 object Teams {
 
-  val all = Seq(Team("Vikings"), Team("Booooo"))
+
 
 }
 
-case class Team(name: String)
+trait Team {
+  val id: String
+  val name: String
+  val numOfWins: Int
+  val numOfLoses: Int
+  val numOfDraws: Int
+  val created: Long
+  val updated: Long
+}
--- slick/src/main/scala/org/zardina/repository/SlickTeamRepository.scala
+++ slick/src/main/scala/org/zardina/repository/SlickTeamRepository.scala
@@ -1,10 +1,11 @@
 package org.zardina.repository
 
 import javax.sql.DataSource
+import org.zardina.{Team, repository}
 import slick.jdbc.JdbcProfile
 import slick.util.AsyncExecutor
 
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.{ExecutionContext, Future}
 
 case class SlickTeam(
   id: String,
@@ -13,7 +14,7 @@ case class SlickTeam(
   numOfLoses: Int,
   numOfDraws: Int,
   created: Long,
-  updated: Long)
+  updated: Long) extends Team
 
 class SlickTeamRepository(dataSource: DataSource)(implicit val profile: JdbcProfile, executionContext: ExecutionContext)
   extends TeamRepository {
@@ -37,5 +38,44 @@ class SlickTeamRepository(dataSource: DataSource)(implicit val profile: JdbcProf
 
   override def createTeam(id: String, name: String): Future[Int] = {
     db.run(table += SlickTeam(id, name, 0, 0, 0, 1L, 1L)).map { x => x }
+  }
+}
+
+object SlickTeam {
+  val teams: Seq[Team] = Seq(
+    ("Arizona Cardinals", "ARI"),
+    ("Chicago Bears", "CHI"),
+    ("Green Bay Packers", "GB"),
+    ("New York Giants", "NYG"),
+    ("Detroit Lions", "DET"),
+    ("Washington Redskins", "WAS"),
+    ("Philadelphia Eagles", "PHI"),
+    ("Pittsburgh Steelers", "PIT"),
+    ("Los Angeles Rams", "LA"),
+    ("San Francisco 49ers", "SF"),
+    ("Cleveland Browns", "CLE"),
+    ("Indianapolis Colts", "IND"),
+    ("Dallas Cowboys", "DAL"),
+    ("Kansas City Chiefs", "KC"),
+    ("Los Angeles Chargers", "LAC"),
+    ("Denver Broncos", "DEN"),
+    ("New York Jets", "NYJ"),
+    ("New England Patriots", "NE"),
+    ("Oakland Raiders", "OAK"),
+    ("Tennessee Titans", "TEN"),
+    ("Buffalo Bills", "BUF"),
+    ("Minnesota Vikings", "MIN"),
+    ("Atlanta Falcons", "ATL"),
+    ("Miami Dolphins", "MIA"),
+    ("New Orleans Saints", "NO"),
+    ("Cincinnati Bengals", "CIN"),
+    ("Seattle Seahawks", "SEA"),
+    ("Tampa Bay Buccaneers", "TB"),
+    ("Carolina Panthers", "CAR"),
+    ("Jacksonville Jaguars", "JAX"),
+    ("Baltimore Ravens", "BAL"),
+    ("Houston Texans", "HOU")
+  ).map {
+    case (name, id) => repository.SlickTeam(id, name, 0, 0, 0, 1L, 2L)
   }
 }
